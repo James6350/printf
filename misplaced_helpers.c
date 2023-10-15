1c646f97bebfc413b026d6d145bc20600508002a
James6350 <nziokamutua6350@gmail.com>
2023-10-15 13:22:32 -0700

    Done

---
+++ misplaced_helpers.c
@@ -0,0 +1,8 @@
+#include "holberton.h"
+/**
+ * _error_ - placeholder for non-existing functions or functions to come
+ */
+void _error_(void)
+{
+	write(1, "Shouldn't have made it here :(\n", 31);
+}
